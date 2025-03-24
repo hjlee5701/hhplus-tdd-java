fd2a4aa54b7b8c8902df03e25486f5d0251c6e69
hjlee5701 <hjlee5701@gmail.com>
2025-03-25 00:44:20 +0900

    feat: 특정 유저의 포인트 조회 기능 구현
    - PointService 클래스에 findUserPointById 메서드 추가
    - 유효하지 않은 userId 인 경우 RuntimeException 으로 예외 처리한다.
      : userId 가 PointHistory 에 없는 경우

---
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,25 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class PointService {
+    private final PointHistoryTable pointHistoryTable;
+    private final UserPointTable userPointTable;
+
+    // 특정 유저의 포인트를 조회하는 기능
+    public UserPoint findUserPointById(long userId) {
+        List<PointHistory> pointHistories = pointHistoryTable.selectAllByUserId(userId);
+
+        if (pointHistories.isEmpty()) {
+            throw new RuntimeException("사용자 포인트 정보가 존재하지 않습니다.");
+        }
+        return userPointTable.selectById(userId);
+    }
+}
