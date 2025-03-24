ed1ac2233f55da7e5c7cb75553f3645b6d107268
hjlee5701 <hjlee5701@gmail.com>
2025-03-25 02:29:06 +0900

    feat: 특정 유저의 포인트를 충전하는 기능 구현
    - 충전 후 보유 포인트가 MAXIMUM_POINT 초과시 예외 발생
    - 포인트 충전 후 pointHistory 에 충전 기록 저장

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -10,6 +10,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class PointService {
+    public static final long MAXIMUM_POINT = 1_000_000;
     private final PointHistoryTable pointHistoryTable;
     private final UserPointTable userPointTable;
 
@@ -27,4 +28,28 @@ public class PointService {
     public List<PointHistory> findHistories(long userId) {
         return pointHistoryTable.selectAllByUserId(userId);
     }
+
+    // 특정 유저의 포인트를 충전하는 기능
+    public UserPoint chargeUserPoint(long userId, long amount) {
+        List<PointHistory> pointHistories = pointHistoryTable.selectAllByUserId(userId);
+
+        if (pointHistories.isEmpty()) {
+            throw new RuntimeException("사용자 포인트 정보가 존재하지 않습니다.");
+        }
+
+        UserPoint userPoint = userPointTable.selectById(userId);
+
+        long totalPoint = amount + userPoint.point();
+        if (totalPoint > MAXIMUM_POINT) {
+            throw new RuntimeException("충전 시 최대 보유 포인트를 초과합니다.");
+        }
+        // 포인트 충전
+        UserPoint chargedUserPoint = userPointTable.insertOrUpdate(userId, totalPoint);
+
+        // 충전 history
+        pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
+
+        return chargedUserPoint;
+    }
+
 }
